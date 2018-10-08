4f391115d004db86110d2fa7fb2c162de5dcbf71
bencoffer <bencoffergit@gmail.com>
2018-10-07 23:06:08 -0400

    Clean up TicTocTimer and TicTocLogger

--- include/epsilib/utility/tictoc.hpp
+++ include/epsilib/utility/tictoc.hpp
@@ -13,13 +13,16 @@ namespace epsilib { namespace utility {
 
 class TicTocTimer {
  public:
-  inline void tic(){
+  inline void tic() {
     m_tic_time = std::chrono::high_resolution_clock::now();
   }
-  inline double toc(){
+  inline void toc() {
     m_toc_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = m_toc_time - m_tic_time;
-    return elapsed.count();  // return elapsed time in seconds
+  }
+  inline double duration() {
+    return static_cast<std::chrono::duration<double>>(
+      m_toc_time - m_tic_time
+    ).count();  // returns elapsed time in seconds
   }
 private:
   std::chrono::high_resolution_clock::time_point m_tic_time;
@@ -34,22 +37,23 @@ public:
     m_tic_toc_timer.tic();
   }
   inline void toc() {
-    m_log.push_back(m_tic_toc_timer.toc());
+    m_tic_toc_timer.toc();
+    m_log.push_back(m_tic_toc_timer.duration());
   };
-  double elapsed() const {return m_log.back();}
-  double mean_elapsed()  const {return epsilib::utility::mean(m_log);} 
-  double max_elapsed()   const {return epsilib::utility::max(m_log);} 
-  double min_elapsed()   const {return epsilib::utility::min(m_log);} 
-  double stdev_elapsed() const {return epsilib::utility::stdev(m_log);} 
+  double duration() const {return m_log.back();}
+  double mean_duration()  const {return epsilib::utility::mean(m_log);} 
+  double max_duration()   const {return epsilib::utility::max(m_log);} 
+  double min_duration()   const {return epsilib::utility::min(m_log);} 
+  double stdev_duration() const {return epsilib::utility::stdev(m_log);} 
   const std::vector<double> &log() const {return m_log;}
   void clear_log() {m_log.clear();}
   std::string stat_string() const {
     std::ostringstream ss;
     ss << m_comment
-       << "  mean: "     << epsilib::utility::to_eng_string(mean_elapsed())
-       << "     max: "   << epsilib::utility::to_eng_string(max_elapsed())
-       << "     min: "   << epsilib::utility::to_eng_string(min_elapsed())
-       << "     stdev: " << epsilib::utility::to_eng_string(stdev_elapsed());
+       << "  mean: "     << epsilib::utility::to_eng_string(mean_duration())
+       << "     min: "   << epsilib::utility::to_eng_string(min_duration())
+       << "     max: "   << epsilib::utility::to_eng_string(max_duration())
+       << "     stdev: " << epsilib::utility::to_eng_string(stdev_duration());
     return ss.str();
   }
 
